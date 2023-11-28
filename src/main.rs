641e95c03ef0945b06d4ae90a1de4b518b3815ff
Mezdelex <alexcondegomez@gmail.com>
2023-11-28 13:05:37 +0100

    feat(rust,channels,threads): added channels and threads example; working with concurrent senders and receivers

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::thread;
+use std::{
+    sync::mpsc::channel,
+    thread::{sleep, spawn},
+    time::Duration,
+};
 
 struct RustConcurrency;
 
@@ -7,7 +11,7 @@ impl RustConcurrency {
         let mut threads = vec![];
 
         for i in 0..10 {
-            let handle = thread::spawn(move || {
+            let handle = spawn(move || {
                 println!("Printing counter {} from new thread", i);
             });
             threads.push(handle);
@@ -21,8 +25,25 @@ impl RustConcurrency {
         // this is going to be printed after the handlers closures
         println!("After thread handlers finished printing.");
     }
+
+    fn using_channels_with_threads() {
+        let (tx, rx) = channel();
+
+        for i in 0..10 {
+            // clone a sender to send to other threads
+            let tx_clone = tx.clone();
+
+            spawn(move || tx_clone.send(format!("Message sent No. {}", i)));
+        }
+
+        for message in rx {
+            println!("{}", message);
+        }
+    }
 }
 
 fn main() {
     RustConcurrency::using_join_handlers();
+    sleep(Duration::from_millis(1000));
+    RustConcurrency::using_channels_with_threads();
 }
